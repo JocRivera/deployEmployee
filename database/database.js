eb2b18a59eff9bebf87fb0c17dba022714362486
JocRivera <jose_mrivera@soy.sena.edu.co>
2024-08-16 10:52:52 -0500

    add mongo cloud

--- database/database.js
+++ database/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-
+const uri = "mongodb+srv://admin:admin@cluster0.wzzlckx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 const connect = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/ecommerce', {
-        })
+        await mongoose.connect(uri, clientOptions);
         console.log('Database connected successfully')
     } catch (error) {
         console.error(error)
